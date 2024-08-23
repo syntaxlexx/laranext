964871a4fceba0ec6b05df918905dd90bc19f782
SyntaxLexx <spencermainaa@gmail.com>
2024-08-23 21:58:20 +0300

    Added github actions deploy script

---
+++ ci-cd/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Running LaraNext update..."
+
+pwd
+git reset --hard
+git clean -fd
+php artisan optimize:clear
+# php artisan icons:clear
+# php artisan filament:clear-cached-components
+git pull --ff
+composer install --no-interaction --prefer-dist --optimize-autoloader -q
+php artisan migrate --force
+export NVM_DIR=~/.nvm
+source ~/.nvm/nvm.sh
+npm install -g pnpm
+pnpm i
+npm run build
+php artisan optimize
+# php artisan icons:cache
+# php artisan filament:cache-components
+echo 'Running commands after update...'
+# php artisan app:update-permissions
+php artisan cache:clear
+# supervisorctl restart laranext-worker:*
+# supervisorctl restart laranext-pulse
+# php artisan pulse:restart
